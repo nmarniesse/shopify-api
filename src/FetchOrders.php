83712ffd20ef021d975d0e25c8e17650f60148f4
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2022-06-17 17:30:37 +0200

    Add orders command

---
+++ src/FetchOrders.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nmarniesse\ShopifyApi;
+
+use Shopify\Auth\FileSessionStorage;
+use Shopify\Clients\HttpResponse;
+use Shopify\Clients\Rest;
+use Shopify\Context;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class FetchOrders extends Command
+{
+    protected static $defaultName = 'shopify:orders:fetch';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setDescription('Test.')
+            ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        Context::initialize(
+            $_ENV['SHOPIFY_API_KEY'],
+            $_ENV['SHOPIFY_API_SECRET'],
+            \explode(',', $_ENV['SHOPIFY_APP_SCOPES'] ?? ''),
+            $_ENV['SHOPIFY_APP_HOST_NAME'],
+            new FileSessionStorage('/tmp/php_sessions'),
+            $_ENV['API_VERSION'],
+            false,
+            false,
+        );
+
+        $client = new Rest($_ENV['SHOPIFY_APP_HOST_NAME'], $_ENV['ACCESS_TOKEN']);
+        $output->writeln($this->prettyfyResponse($client->get('orders')));
+
+        return Command::SUCCESS;
+    }
+
+    private function prettyfyResponse(HttpResponse $response): string
+    {
+        try {
+            $decoded = \json_decode($response->getBody()->getContents(), true, JSON_THROW_ON_ERROR);
+
+            return  \json_encode($decoded, JSON_PRETTY_PRINT);
+        } catch (\JsonException $e) {
+            return $response->getBody()->getContents();
+        }
+    }
+}
